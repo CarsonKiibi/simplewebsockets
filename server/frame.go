963c987cad7ac22ff087e5792b434263c0f8d520
CarsonKiibi <carrotscarson@gmail.com>
2025-05-06 18:52:19 -0700

    broken

--- server/frame.go
+++ server/frame.go
@@ -1,10 +1,7 @@
 package server
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
-	"log"
 )
 
 // must be encoded as raw bytes before being sent over tcp
@@ -20,13 +17,51 @@ type Frames struct {
 	MsgFrames []Frame
 }
 
-func (fs Frames) FrameToPayload() []byte {
-	var buf bytes.Buffer
-	for f := range fs.MsgFrames {
+func (fs Frames) FrameToBytes() []byte {
+	// var buf bytes.Buffer
+	for _, f := range fs.MsgFrames {
+		PayloadLength := len(f.Payload)
+		switch f.Opcode {
+		case 0x0:
+			fmt.Println("0x0: Continue Frame")
+		case 0x1:
+			fmt.Println("0x1: Text Frame")
+		case 0x2:
+			fmt.Println("0x2: Binary Frame")
+		case 0x3:
+			fmt.Println("0x3: Reserved Frame")
+		case 0x4:
+			fmt.Println("0x4: Reserved Frame")
+		case 0x5:
+			fmt.Println("0x5: Reserved Frame")
+		case 0x6: 
+			fmt.Println("0x6: Reserved Frame")
+		case 0x7:
+			fmt.Println("0x7: Reserved Frame")
+		case 0x8:
+			fmt.Println("0x8: Connection Closed")
+		case 0x9:
+			fmt.Println("0x9: Ping")
+		case 0xa:
+			fmt.Println("0xa: Pong")
+		case 0xb:
+			fmt.Println("0xb: Reserved control frame")
+		case 0xc: 
+			fmt.Println("0xc: Reserved control frame")
+		case 0xd:
+			fmt.Println("0xd: Reserved control frame")
+		case 0xe:
+			fmt.Println("0xe: Reserved control frame")
+		case 0xf:
+			fmt.Println("0xf: Reserved control frame")
+		default:
+			fmt.Println("Impossible Opcode")
+		}
 		if (f.FIN == true) {
-
+			
 		}
 	}
+	return []byte{}
 } 
 
 // Turns payload (byte array) into frames
@@ -35,7 +70,6 @@ func PayloadToFrames([]byte) []Frame {
 
 	return []Frame{}
 }
-``
 func PrintBits() {
 
 }
---
+++ server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"net"
+	"sync"
+	"time"
+)
+
+type Connection struct {
+	conn net.Conn 
+	writeMx sync.Mutex
+
+	closed bool 
+
+	onMessage func([]byte)
+	onClose func()
+
+	readBuf []byte 
+	writeBuf []byte 
+	maxSize int64
+}
+
+type Server struct {
+	connections map[*Connection]bool
+	connectionsMx sync.RWMutex
+
+	maxMessageSize int64 
+	readTimeout time.Duration
+	writeTimeout time.Duration
+
+	onConnect func(*Connection)
+	onDisconnect func(*Connection)
+	onError func(*Connection, error)
+}
+
+func NewServer() *Server {
+
+}
