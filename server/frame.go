3ad0bae2838f8c5eb1383c0a9297d6c917112c74
Carson <carrotscarson@gmail.com>
2025-05-11 22:42:21 -0700

    baba

--- server/frame.go
+++ server/frame.go
@@ -1,16 +1,15 @@
 package server
 
-import (
-	// "fmt"
-)
+// "fmt"
 
 // must be encoded as raw bytes before being sent over tcp
 type Frame struct {
-	FIN bool // 1-bit flag
-	Opcode byte
-	Mask bool // 1-bit flag
-	MaskKey [4]byte
-	Payload []byte
+	FIN           bool // 1-bit flag
+	Opcode        byte
+	Mask          bool // 1-bit flag
+	MaskKey       [4]byte
+	Payload       []byte
+	PayloadLength int64
 }
 
 type Frames struct {
@@ -27,17 +26,38 @@ func (fs Frames) FrameToBytes() []byte {
 			frameSize += 4 // add mask if it exists
 		}
 
-		frameSize += len(f.Payload) // add payload length
+		frameSize += int(f.PayloadLength) // add payload length
+
+		maskOffset := 0
+
+		if (f.PayloadLength >= 126) && (f.PayloadLength <= 65535) {
+			frameSize += 1
+			maskOffset += 1
+		} else if f.PayloadLength > 65536 {
+			frameSize += 4
+			maskOffset += 4
+		}
 
 		currFrame := make([]byte, frameSize)
-		
-		if (f.FIN) {
+
+		if f.FIN {
 			currFrame[0] |= 0x80
 		}
 
 		currFrame[0] |= f.Opcode
 
-		// --- FIRST BYTE IS SET --- 
+		if f.Mask {
+			currFrame[1] |= 0x80
+		}
+
+		if f.Mask {
+			currFrame[2+maskOffset] |= f.MaskKey[0]
+			currFrame[3+maskOffset] |= f.MaskKey[1]
+			currFrame[4+maskOffset] |= f.MaskKey[2]
+			currFrame[5+maskOffset] |= f.MaskKey[3]
+		}
+
+		// --- FIRST BYTE IS SET ---
 
 		// switch f.Opcode {
 		// case 0x0:
@@ -52,7 +72,7 @@ func (fs Frames) FrameToBytes() []byte {
 		// 	fmt.Println("0x4: Reserved Frame")
 		// case 0x5:
 		// 	fmt.Println("0x5: Reserved Frame")
-		// case 0x6: 
+		// case 0x6:
 		// 	fmt.Println("0x6: Reserved Frame")
 		// case 0x7:
 		// 	fmt.Println("0x7: Reserved Frame")
@@ -64,7 +84,7 @@ func (fs Frames) FrameToBytes() []byte {
 		// 	fmt.Println("0xa: Pong")
 		// case 0xb:
 		// 	fmt.Println("0xb: Reserved control frame")
-		// case 0xc: 
+		// case 0xc:
 		// 	fmt.Println("0xc: Reserved control frame")
 		// case 0xd:
 		// 	fmt.Println("0xd: Reserved control frame")
@@ -77,15 +97,13 @@ func (fs Frames) FrameToBytes() []byte {
 		// }
 	}
 	return []byte{}
-} 
+}
 
 // Turns payload (byte array) into frames
 func PayloadToFrames([]byte) []Frame {
-
 
 	return []Frame{}
 }
 func PrintBits() {
 
 }
-
